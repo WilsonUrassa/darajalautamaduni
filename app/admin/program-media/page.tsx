213d15a1c2be94e6374afed75c38df61dafe483b
WilsonUrassa <58864204+WilsonUrassa@users.noreply.github.com>
2026-08-27 20:03:44 +0300

    Add programme media admin page

---
+++ app/admin/program-media/page.tsx
@@ -0,0 +1,2 @@
+import ProgramMediaManager from "@/components/ProgramMediaManager";
+export default function ProgramMediaPage(){return <main className="min-h-screen bg-paper p-6 md:p-10 pt-28"><div className="mx-auto max-w-[1100px]"><ProgramMediaManager/></div></main>}
